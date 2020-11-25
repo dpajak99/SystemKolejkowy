6b79e6199fc65a30b2ea998f01bf776810f1b42e
Robert-Mal <33180@student.pwsztar.edu.pl>
2020-11-25 17:55:08 +0100

    Initiated project

---
+++ cmake-build-debug/CMakeFiles/system_kolejkowy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/system_kolejkowy.dir/main.cpp.obj"
+  "libsystem_kolejkowy.dll.a"
+  "system_kolejkowy.exe"
+  "system_kolejkowy.exe.manifest"
+  "system_kolejkowy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/system_kolejkowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
